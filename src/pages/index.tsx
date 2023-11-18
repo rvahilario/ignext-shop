ffe8cc232c54a12d6dddbe699eb6b8927d6e03ec
renan.hilario <rvahilario@gmail.com>
2023-11-18 20:32:09 -0300

    refact: adjust code style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -116,6 +116,7 @@ function Arrow({ isDisabled, isLeft, onClick }: ArrowProps) {
       {isLeft && (
         <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
       )}
+
       {!isLeft && (
         <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
       )}
